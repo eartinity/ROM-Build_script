224dff2894c10c1adc42f925243fa854aaa4ff9d
Priya Ranjan Samal <140955751+eartinity@users.noreply.github.com>
2023-10-03 07:22:17 +0530

    script: Add setup and source sync commands

--- build.sh
+++ build.sh
@@ -1,16 +1,34 @@
 #!/usr/bin/env bash
 
+# Setup environment
+mkdir ~/bin
+PATH=~/bin:$PATH
+cd ~/bin
+curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+git clone https://github.com/akhilnarang/scripts.git scripts
+cd scripts
+bash setup/android_build_env.sh
+
+# Sync Source
+git config --global user.name "eartinity" && git config --global user.email "priyaranjan24samal@gmail.com"
+mkdir pe
+cd pe
+ln -s /usr/bin/python3 /usr/bin/python
+repo init https://github.com/PixelExperience/manifest.git -b thirteen -y
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+
 # Sync Trees
 git clone  https://github.com/eartinity/device_realme_r5x device/realme/r5x -b awaken
 git clone  https://github.com/eartinity/vendor_realme_r5x vendor/realme/r5x -b thirteen
-git clone  https://github.com/Amritorock/kernel_realme_r5x -b OSS kernel/realme/r5x
+git clone  https://github.com/eartinity/kernel_realme_trinket -b labs-dynamic kernel/realme/r5x
 
 # Start Build
 source build/envsetup.sh
-lunch awaken_r5x-userdebug
+lunch aosp_r5x-userdebug
 make bacon -j$(nproc --all)
 
 # Upload
 cd out/target/product/r5x
 curl -sL https://git.io/file-transfer | sh
-./transfer wet awaken*.zip￼Enter
+./transfer wet aosp*.zip￼Enter
