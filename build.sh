b4d75fbab1d9b66a48ca38cec2faf3cde133836d
Priya Ranjan Samal <140955751+eartinity@users.noreply.github.com>
2023-11-25 07:09:18 +0530

    Revamp some changes

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@
 mkdir ~/bin
 PATH=~/bin:$PATH
 cd ~/bin
+apt update -y
+apt install curl -y
 curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
 git clone https://github.com/eartinity/scripts.git scripts
@@ -12,14 +14,12 @@ bash setup/android_build_env.sh
 
 # Sync Source
 git config --global user.name "eartinity" && git config --global user.email "priyaranjan24samal@gmail.com"
-mkdir pe
-cd pe
-ln -s /usr/bin/python3 /usr/bin/python
+mkdir pe && cd pe
 repo init https://github.com/PixelExperience/manifest.git -b thirteen -y
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 # Sync Trees
-git clone  https://github.com/eartinity/device_realme_r5x device/realme/r5x -b awaken
+git clone  https://github.com/eartinity/device_realme_r5x device/realme/r5x -b pe
 git clone  https://github.com/eartinity/vendor_realme_r5x vendor/realme/r5x -b thirteen
 git clone  https://github.com/eartinity/kernel_realme_trinket -b labs-dynamic kernel/realme/r5x
 
